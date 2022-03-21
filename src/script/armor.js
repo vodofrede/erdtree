0a45bb25d068bedb1afe6bdb4788e1a7f2ed84d3
vodofrede <frederik@palmoe.dk>
2022-03-21 20:06:45 +0100

    mvp armor optimizer

--- src/script/armor.js
+++ src/script/armor.js
@@ -1,32 +1,27 @@
-const HELMETS = fetch("/data/helmets.json")
+const HELMETS = fetch("/data/armor/helmets.json")
     .then(response => response.json())
     .then(data => data.helmets)
     .catch(error => console.log(error));
-const CHESTPIECES = fetch("/data/chestpieces.json")
+const CHESTPIECES = fetch("/data/armor/chestpieces.json")
     .then(response => response.json())
     .then(data => data.chestpieces)
     .catch(error => console.log(error));
-const GAUNTLETS = fetch("/data/gauntlets.json")
+const GAUNTLETS = fetch("/data/armor/gauntlets.json")
     .then(response => response.json())
     .then(data => data.gauntlets)
     .catch(error => console.log(error));
-const LEGGINGS = fetch("/data/leggings.json")
+const LEGGINGS = fetch("/data/armor/leggings.json")
     .then(response => response.json())
     .then(data => data.leggings)
     .catch(error => console.log(error));
 
-let sortedHelmets;
-let sortedChestplates;
-let sortedGauntlets;
-let sortedLeggings;
-
-// const SortingMethod = {
-//     AVERAGE,
-//     POISE,
-//     PHYSICAL,
-//     ELEMENTAL,
-//     IMMUNITIES,
-// };
+// armor combination lists
+const PHYSICAL = fetch("/data/armor/combinations/physical.json")
+    .then(response => response.json())
+    .catch(error => console.log(error));
+const POISE = fetch("/data/armor/combinations/poise.json")
+    .then(response => response.json())
+    .catch(error => console.log(error));
 
 async function init() {
     // populate filter selects
@@ -35,32 +30,63 @@ async function init() {
     populateSelect("locked-option", "select-gauntlets", await GAUNTLETS);
     populateSelect("locked-option", "select-leggings", await LEGGINGS);
 
-    // precompute and sort list of armor pieces
-
-}
-
-async function update() {
-    // let sorted = sortedCombinations();
-}
-
-function updateSortingMethod() {
     update();
 }
 
-function fitness(item, order) {
-    // return fitness of item based on given order
-    // switch (order) {
-    //     case SortingMethod.AVERAGE:
-    //         return item.defenses.reduce((total, value) => total + value, 0) / item.defenses.length;
-    //     case SortingMethod.PHYSICAL:
-    //         break;
-    //     case SortingMethod.ELEMENTAL:
-    //         break;
-    //     case SortingMethod.IMMUNITIES:
-    //         break;
-    //     default:
-    //         console.log("error pls fix");
-    // }
+async function update() {
+    // get budget and sorting order
+    let budget = equipLoadBudget();
+    let sortBy = currentSortBy();
+
+    // find sets under budget
+    let best = await findUnderBudget(budget, sortBy, 3);
+
+    // show best sets under budget
+    Array.from(document.getElementsByClassName("sort-result")).forEach(elem => elem.parentNode.removeChild(elem));
+    populateResults("sort-result", "sort-results", best);
+}
+
+function clearEquipment() {
+    [...document.getElementsByName("locked-equipment")].forEach(select => select.selectedIndex = 0);
+}
+
+function equipLoadBudget() {
+    let rollType = [...document.getElementsByName("roll-type")].find(elem => elem.checked).id;
+    let rollModifier;
+    switch (rollType) {
+        case "fast-roll":
+            rollModifier = 0.3;
+            break;
+        case "normal-roll":
+            rollModifier = 0.7;
+            break;
+        case "fat-roll":
+            rollModifier = 1.0;
+            break;
+    }
+
+    return (parseInt(document.getElementById("max-equip-load").value) - parseInt(document.getElementById("current-equip-load").value)) * rollModifier;
+}
+
+function currentSortBy() {
+    return [...document.getElementsByName("sorting-order")].find(elem => elem.checked).id;
+}
+
+async function findUnderBudget(budget, sortBy, amount) {
+    let sets;
+
+    switch (sortBy) {
+        case "greatest-physical":
+            sets = (await PHYSICAL);
+            break;
+        case "greatest-poise":
+            sets = (await POISE);
+            break;
+    }
+
+    let first = sets.findIndex(set => set.weight <= budget);
+
+    return sets.slice(first, first + amount);
 }
 
 function populateSelect(templateId, destinationId, items) {
@@ -77,6 +103,28 @@ function populateSelect(templateId, destinationId, items) {
     });
 }
 
-function clearEquipment() {
-    [...document.getElementsByName("locked-equipment")].forEach(select => select.selectedIndex = 0);
+async function populateResults(templateId, destinationId, sets) {
+    let helmets = await HELMETS;
+    let chestpieces = await CHESTPIECES;
+    let gauntlets = await GAUNTLETS;
+    let leggings = await LEGGINGS;
+
+    let template = document.getElementById(templateId);
+    let destination = document.getElementById(destinationId);
+
+    sets.forEach(set => {
+        let clone = template.content.cloneNode(true);
+
+        let li = clone.children[0];
+        let table = li.children[0];
+        let tbody = table.children[1];
+        let rows = tbody.children;
+
+        rows[0].children[1].innerText = helmets.find(helmet => helmet.id == set.helmet).name;
+        rows[1].children[1].innerText = chestpieces.find(chest => chest.id == set.chestpiece).name;
+        rows[2].children[1].innerText = gauntlets.find(gauntlets => gauntlets.id == set.gauntlets).name
+        rows[3].children[1].innerText = leggings.find(leggings => leggings.id == set.leggings).name
+
+        destination.appendChild(clone);
+    });
 }
