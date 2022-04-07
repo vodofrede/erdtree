8cc402f5fdf1f542cd1f9cd8dac46ed278444bb0
vodofrede <frederik@palmoe.dk>
2022-04-07 17:06:54 +0200

    added buffable information

--- src/script/planner.js
+++ src/script/planner.js
@@ -1,96 +1,130 @@
-const HELMETS = fetch("/data/armor/helmets.json")
-    .then(response => response.json())
-    .catch(error => console.log(error));
-const CHESTPIECES = fetch("/data/armor/chestpieces.json")
-    .then(response => response.json())
-    .catch(error => console.log(error));
-const GAUNTLETS = fetch("/data/armor/gauntlets.json")
-    .then(response => response.json())
-    .catch(error => console.log(error));
-const LEGGINGS = fetch("/data/armor/leggings.json")
-    .then(response => response.json())
-    .catch(error => console.log(error));
-const WEAPONS = fetch("/data/weapons.json")
-    .then(response => response.json())
-    .catch(error => console.log(error));
-const TALISMANS = fetch("/data/talismans.json")
-    .then(response => response.json())
-    .catch(error => console.log(error));
+let HELMETS;
+let CHESTPIECES;
+let GAUNTLETS;
+let LEGGINGS;
+let WEAPONS;
+let TALISMANS;
+let EQUIPMENT;
+let CLASSES;
+let INFUSIONS;
 
-const CLASSES = fetch("/data/classes.json")
-    .then(response => response.json())
-    .catch(error => console.log(error));
+let startingClass = "wretch";
+let popupActive = false;
 
-let startingClass;
+let populate = (select, items) => items.forEach(item => select.options.add(new Option(item.name, item.id)));
+let selected = select => select.options[select.selectedIndex];
 
 async function init() {
-    await populate();
-    await update();
-}
+    HELMETS = await fetch("/data/helmets.json").then(response => response.json());
+    CHESTPIECES = await fetch("/data/chestpieces.json").then(response => response.json());
+    GAUNTLETS = await fetch("/data/gauntlets.json").then(response => response.json());
+    LEGGINGS = await fetch("/data/leggings.json").then(response => response.json());
+    TALISMANS = await fetch("/data/talismans.json").then(response => response.json());
+    WEAPONS = await fetch("/data/weapons.json").then(response => response.json());
+    EQUIPMENT = { ...HELMETS, ...CHESTPIECES, ...GAUNTLETS, ...LEGGINGS, ...TALISMANS, ...WEAPONS };
 
-async function update() {
-    // get current chosen starting class
-    let classSelect = document.getElementById("class");
-    let currentClass = await (CLASSES.then(classes => {
-        return classes.find(c => c.id == classSelect.options[classSelect.selectedIndex].id);
-    }));
+    CLASSES = await fetch("/data/classes.json").then(response => response.json());
+    INFUSIONS = await fetch("/data/infusions.json").then(response => response.json());
 
-    // get virtual stats
-    let items = await itemStats();
-    console.log(items);
-
-    // update statistics screen
-    let prevClass = startingClass ?? currentClass;
-    [...document.getElementsByName("stat")].forEach((stat, i) => {
-        stat.value = ((stat.value == "" || (stat.value - parseInt(items[i])) == prevClass.stats[i]) ? currentClass.stats[i] + parseInt(items[i]) : parseInt(stat.value) + parseInt(items[i]));
+    // populate selects
+    [...document.getElementsByName("weapon")].forEach(select => {
+        populate(select, Object.values(WEAPONS));
     });
-    document.getElementById("level").value = [...document.getElementsByName("stat")]
-        .reduce((total, stat) => total + parseInt(stat.value), 0) - 79;
-    startingClass = currentClass;
-}
+    populate(document.getElementById("helmet"), Object.values(HELMETS));
+    populate(document.getElementById("chestpiece"), Object.values(CHESTPIECES));
+    populate(document.getElementById("gauntlets"), Object.values(GAUNTLETS));
+    populate(document.getElementById("leggings"), Object.values(LEGGINGS));
+    [...document.getElementsByName("talisman")].forEach(select => {
+        populate(select, Object.values(TALISMANS));
+    });
 
-function reset(area) {
-    switch (area) {
-        case "equipment":
-            let types = ["helmets", "chestpieces", "gauntlets", "leggings", "weapons", "talismans"];
-            types.forEach(ty => {
-                let selects = [...document.getElementsByName(ty)];
-                selects.forEach(select => {
-                    select.selectedIndex = 0;
-                })
-            })
-            break;
-        case "statistics":
-            document.getElementById("class").selectedIndex = 0;
-            [...document.getElementsByName("stat")].forEach(stat => stat.value = 10);
-            break;
-        default:
-            break;
-    }
+    // override ctrl/cmd+s
+    document.addEventListener("keydown", event => {
+        if (!(event.isComposing || event.code === "229")) {
+            if ((event.ctrlKey || event.metaKey) && event.key == "s") {
+                event.preventDefault();
+                exportBuild();
+            }
+        }
+    });
+
     update();
 }
 
-async function itemStats() {
-    let helmet = document.getElementById("helmet");
-    let talismans = document.getElementsByName("talismans");
-    let ids = [helmet, ...talismans]
-        .map(elem => elem.options[elem.selectedIndex].value)
-        .filter(id => !id.startsWith("no-"));
-    let relevant = [await HELMETS, await TALISMANS].flat().filter(item => ids.includes(item.id));
+function update() {
+    // get current chosen starting class
+    let classSelect = document.getElementById("class");
+    let currentClass = CLASSES.find(c => c.id == classSelect.options[classSelect.selectedIndex].id);
 
-    return relevant.reduce((total, item) => total.map((stat, i) => stat += item.stats[i]), [0, 0, 0, 0, 0, 0, 0, 0]);
+    // item stats
+    let itemStats = [...document.getElementsByName("armor"), ...document.getElementsByName("talisman")].reduce(
+        (stats, select) => {
+            let item = EQUIPMENT[select.options[select.selectedIndex].value];
+            return stats.map((stat, i) => (item.stats != undefined ? stat + item.stats[i] : stat));
+        },
+        [0, 0, 0, 0, 0, 0, 0, 0],
+    );
+
+    if (currentClass != startingClass) {
+        // reset stats if other class was chosen
+        [...document.getElementsByName("final")].forEach((el, i) => {
+            el.value = Math.min(currentClass.stats[i] + itemStats[i], 99);
+        });
+        startingClass = currentClass;
+    } else {
+        // clamp stat ranges
+        [...document.getElementsByName("final")].forEach((el, i) => {
+            el.value = Math.min(Math.max(currentClass.stats[i] + itemStats[i], el.value), 99);
+        });
+    }
+
+    // show only available infusions for each weapon
+    let weapons = [...document.getElementsByName("weapon")]
+        .map(sl => sl.options[sl.selectedIndex].value)
+        .map(id => WEAPONS[id]);
+    let infusions = [...document.getElementsByName("infusion")];
+    infusions
+        .map((inf, i) => [weapons[i], inf])
+        .forEach(([weapon, infusionSelect]) => {
+            infusionSelect.length = 0;
+            Object.keys(weapon.infusions).forEach(infusionId => {
+                let infusion = INFUSIONS[infusionId];
+                infusionSelect.options.add(new Option(infusion.name, infusion.id));
+            });
+        });
+
+    // update infusion icons
+    [...document.getElementsByName("infusion")].forEach(select => {});
+
+    // calculate total level
+    // document.getElementById("final-level").value = currentClass.level + additional.reduce((sum, n) => sum + n);
 }
 
-async function populate() {
-    Promise.all([HELMETS, CHESTPIECES, GAUNTLETS, LEGGINGS, WEAPONS, TALISMANS]).then(itemTypes => {
-        itemTypes.forEach((itemType, i) => {
-            let ty = ["helmets", "chestpieces", "gauntlets", "leggings", "weapons", "talismans"][i];
-            let selects = [...document.getElementsByName(ty)];
-            selects.forEach(select => {
-                itemType.forEach(item => {
-                    select.options.add(new Option(item.name, item.id));
-                })
-            })
-        });
-    });
+function importBuild(buildString) {}
+
+function exportBuild() {
+    let classSelect = document.getElementById("class");
+    let itemStats = [...document.getElementsByName("armor"), ...document.getElementsByName("talisman")].reduce(
+        (stats, select) => {
+            let item = EQUIPMENT[selected(select).value];
+            return stats.map((stat, i) => (item.stats != undefined ? stat + item.stats[i] : stat));
+        },
+        [0, 0, 0, 0, 0, 0, 0, 0],
+    );
+    console.log(itemStats);
+    let build = {
+        name: document.getElementById("name").value,
+        class: classSelect.options[classSelect.selectedIndex].value,
+        stats: [...document.getElementsByName("final")].map((stat, i) => stat.value - itemStats[i]),
+        weapons: [...document.getElementsByName("weapon")].map(selected).map(w => w.value),
+        infusions: [...document.getElementsByName("infusion")].map(selected).map(i => i.value),
+        armor: [...document.getElementsByName("armor")].map(selected).map(a => a.value),
+        talismans: [...document.getElementsByName("talisman")].map(selected).map(t => t.value),
+    };
+
+    let file = new Blob([JSON.stringify(build, null, 4)], { type: "text/plain" });
+    let link = document.createElement("a");
+    link.href = URL.createObjectURL(file);
+    link.download = "build-" + (build.name || build.class.toLowerCase()) + ".json";
+    link.click();
 }
