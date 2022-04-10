ea572e7087186324f70713a268e1c50fd8c22012
Frederik Palmø <frederik@palmoe.dk>
2022-04-10 23:53:04 +0200

    added favicon and updated sitemap

--- src/script/class.js
+++ src/script/class.js
@@ -1,64 +1,63 @@
-const CLASSES = fetch("/data/classes.json")
-    .then((response) => response.json())
-    .catch((error) => console.log(error));
-const TALISMANS = fetch("/data/talismans.json")
-    .then((response) => response.json())
-    .catch((error) => console.log(error));
-const HELMETS = fetch("/data/helmets.json")
-    .then((response) => response.json())
-    .catch((error) => console.log(error));
+let CLASSES;
+let TALISMANS;
+let HELMETS;
 
 const STAT_SHORT_NAMES = ["vig.", "mnd.", "end.", "str.", "dex.", "int.", "fth.", "arc."];
 
+const selected = select => select.options[select.selectedIndex];
+
 async function init() {
+    // fetch and await data sources
+    CLASSES = await fetch("/data/classes.json").then(response => response.json());
+    TALISMANS = await fetch("/data/talismans.json").then(response => response.json());
+    HELMETS = await fetch("/data/helmets.json").then(response => response.json());
+
     // populate helmet select
-    let helmets = Object.values(await HELMETS);
-    helmets = helmets.filter((helmet) => helmet.stats != null);
     let destination = document.getElementById("helmet");
-    helmets.forEach((helmet) => {
-        destination.options.add(new Option(helmet.name, helmet.id));
-    });
+    Object.values(HELMETS)
+        .filter(helmet => helmet.stats != null)
+        .forEach(helmet => destination.options.add(new Option(helmet.name, helmet.id)));
 
     // populate talisman list
-    let talismans = Object.values(await TALISMANS);
-    talismans = talismans.filter((talisman) => talisman.stats != null && !talisman.id.includes("scar"));
-
     let template = document.getElementById("talisman");
     destination = document.getElementById("talismans");
-    talismans.forEach((item) => {
-        let clone = template.content.cloneNode(true);
-        let li = clone.children[0];
 
-        let input = li.children[0].children[0];
-        let label = li.children[0].children[1];
-        let aside = li.children[1];
+    Object.values(TALISMANS)
+        .filter(talisman => talisman.stats != null && !talisman.id.includes("scar"))
+        .forEach(item => {
+            let clone = template.content.cloneNode(true);
+            let li = clone.children[0];
 
-        input.id = item.id;
-        input.value = item.id;
-        label.htmlFor = item.id;
-        label.innerHTML = item.name;
-        aside.innerHTML = statsDescription(item.stats);
+            let input = li.children[0].children[0];
+            let label = li.children[0].children[1];
+            let aside = li.children[1];
 
-        destination.appendChild(clone);
-    });
+            input.id = item.id;
+            input.value = item.id;
+            label.htmlFor = item.id;
+            label.innerHTML = item.name;
+            aside.innerHTML = statsDescription(item.stats);
+
+            destination.appendChild(clone);
+        });
 
     update();
 }
 
-async function update() {
+function update() {
     // get inputted stats, clamp value to 0..99
-    let total = [...document.getElementsByName("total")].map((elem) => {
+    let total = [...document.getElementsByName("total")].map(elem => {
         elem.value = Math.min(Math.max(elem.value, 0), 99) || null;
         return parseInt(elem.value) || 0;
     });
 
     // get added stats from items
-    let items = itemStats(Object.values(await TALISMANS).concat(Object.values(await HELMETS)));
+    let items = itemStats([...Object.values(TALISMANS), ...Object.values(HELMETS)]);
 
     // calculate best class
     let sorted = sortClasses(
-        Object.values(await CLASSES),
-        total.map((stat, i) => stat - items[i])
+        Object.values(CLASSES),
+        total.map((stat, i) => stat - items[i]),
     );
     let best = sorted[0];
 
@@ -80,7 +79,7 @@ async function update() {
     let destination = document.getElementById("classes");
     destination.innerHTML = "";
     let template = document.getElementById("class");
-    sorted.forEach((c) => {
+    sorted.forEach(c => {
         let clone = template.content.cloneNode(true);
         let li = clone.children[0];
         let span = li.children[0];
@@ -94,41 +93,40 @@ async function update() {
 
     // update talismans
     let talismans = [...document.getElementsByName("talisman")];
-
-    if (talismans.filter((checkbox) => checkbox.checked).length >= 4) {
-        talismans.forEach((checkbox) => (checkbox.disabled = !checkbox.checked));
+    if (talismans.filter(checkbox => checkbox.checked).length >= 4) {
+        talismans.forEach(checkbox => (checkbox.disabled = !checkbox.checked));
     } else {
-        talismans.forEach((checkbox) => (checkbox.disabled = false));
+        talismans.forEach(checkbox => (checkbox.disabled = false));
     }
 }
 
-function sortClasses(classes, desiredStats) {
-    let deltas = classes.map((c) => {
-        c.total = c.level + statDelta(c.stats, desiredStats);
-        return c;
-    });
-    deltas.sort((a, b) => a.total - b.total);
-    return deltas;
+function delta(classStats, final) {
+    return classStats.map((stat, i) => (stat < final[i] ? final[i] - stat : 0)).reduce((total, n) => total + n);
 }
 
-function statDelta(classStats, desiredStats) {
-    return classStats.map((e, i) => (e < desiredStats[i] ? desiredStats[i] - e : 0)).reduce((total, n) => total + n);
+function sortClasses(classes, desiredStats) {
+    return classes
+        .map(c => {
+            c.total = c.level + delta(c.stats, desiredStats);
+            return c;
+        })
+        .sort((a, b) => a.total - b.total);
 }
 
 function itemStats(relevantItems) {
-    let helmet = document.getElementById("helmet").options[document.getElementById("helmet").selectedIndex].value;
-    let talismans = [...document.getElementsByName("talisman")].filter((t) => t.checked).map((t) => t.value);
+    let helmet = selected(document.getElementById("helmet")).value;
+    let talismans = [...document.getElementsByName("talisman")].filter(t => t.checked).map(t => t.value);
 
     ids = [helmet, ...talismans];
-    let relevant = relevantItems.filter((item) => ids.includes(item.id));
-
-    return relevant.reduce((total, item) => total.map((stat, i) => (stat += item.stats[i])), [0, 0, 0, 0, 0, 0, 0, 0]);
+    return relevantItems
+        .filter(item => ids.includes(item.id))
+        .reduce((total, item) => total.map((stat, i) => (stat += item.stats[i])), [0, 0, 0, 0, 0, 0, 0, 0]);
 }
 
-function reset() {
-    document.getElementsByName("total").forEach((elem) => (elem.value = null));
+function resetAll() {
+    document.getElementsByName("total").forEach(elem => (elem.value = null));
     document.getElementById("helmet").selectedIndex = 0;
-    [...document.getElementsByName("talisman")].forEach((elem) => (elem.checked = false));
+    [...document.getElementsByName("talisman")].forEach(elem => (elem.checked = false));
 
     update();
 }
@@ -137,8 +135,4 @@ function statsDescription(stats) {
     return stats.reduce((total, stat, i) => {
         return stat ? total + " +" + stat + STAT_SHORT_NAMES[i] : total;
     }, "");
-}
-
-function clamp(n, min, max) {
-    return Math.min(Math.max(n, min), max);
 }
