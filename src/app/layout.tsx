09ea36d14b200498afae4aef1ddb7b308139c56d
Cameron Chrobocinski <cameronchrobo@gmail.com>
2024-09-08 15:52:56 -0500

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata, Viewport } from "next";
 import Link from "next/link";
 import "./globals.css";
@@ -27,6 +28,7 @@ export default function RootLayout({
             <body>
                 <NavLinks />
                 {children}
+                <Analytics />
                 <footer>
                     <h5>
                         Erdtree Planner (
